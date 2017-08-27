5656f1fdd33c4d32fe7fdd0ad99e55a30a3af598
ParkYeoungJun <dudwns4971@gmail.com>
2017-08-28 02:04:43 +0900

    toolbar, searchbar done

---
+++ app/src/main/java/com/example/park/yapp_1team/activities/LocationSetupActivity.java
@@ -0,0 +1,19 @@
+package com.example.park.yapp_1team.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.park.yapp_1team.R;
+
+/**
+ * Created by Park on 2017-08-27.
+ */
+
+public class LocationSetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_time_setup);
+    }
+}
