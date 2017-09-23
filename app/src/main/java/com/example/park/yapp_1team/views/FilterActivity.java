5a7bc6ba556da85857723fcd9ca2795434b4f53f
TaeYoungKo <kaun1429@gmail.com>
2017-09-24 03:21:11 +0900

    filter

---
+++ app/src/main/java/com/example/park/yapp_1team/views/FilterActivity.java
@@ -0,0 +1,15 @@
+package com.example.park.yapp_1team.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.park.yapp_1team.R;
+
+public class FilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+    }
+}
